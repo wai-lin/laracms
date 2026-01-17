a659e09d5a2650921dce21a1ad0c0885b89e00a1
wai-lin <dev.wailin@gmail.com>
2026-01-17 23:35:09 +0700

    feat: integrate Sentry for error tracking and performance monitoring

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,20 @@
 import Quill from "quill";
 import "quill/dist/quill.snow.css";
+import * as Sentry from "@sentry/browser";
 
 window.Quill = Quill;
+
+window.onload = function () {
+    Sentry.init({
+        dsn: "https://65cf5958a1be127c93b6374310e670a0@o4510726529810432.ingest.de.sentry.io/4510726531252304",
+        // Setting this option to true will send default PII data to Sentry.
+        // For example, automatic IP address collection on events
+        sendDefaultPii: true,
+        integrations: [
+            Sentry.replayIntegration()
+        ],
+        // Session Replay
+        replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+        replaysOnErrorSampleRate: 1.0 // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+    });
+};
